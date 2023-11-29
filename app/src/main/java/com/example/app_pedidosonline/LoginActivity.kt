b2380b1f93bf1c2fcb6cdfbbd215de932f09eec4
Felipe <felipetermometria@hotmail.com>
2023-11-29 09:50:40 -0300

    Login e senha inseridos - Logica

--- app/src/main/java/com/example/app_pedidosonline/LoginActivity.kt
+++ app/src/main/java/com/example/app_pedidosonline/LoginActivity.kt
@@ -10,40 +10,40 @@ import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-            val emailEditText: EditText = findViewById(R.id.editTextEmail)
-            val usuarioEditText: EditText = findViewById(R.id.editTextUsuario)
-            val loginButton: Button = findViewById(R.id.btnLogin)
+        val emailEditText: EditText = findViewById(R.id.editTextEmail)
+        val senhaEditText: EditText = findViewById(R.id.editTextSenha)
+        val loginButton: Button = findViewById(R.id.btnLogin)
 
-            loginButton.setOnClickListener {
-                val email = emailEditText.text.toString().trim()
-                val usuario = usuarioEditText.text.toString().trim()
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            val senha = senhaEditText.text.toString().trim()
 
-                if (isValidInput(email, usuario)) {
-                    // Autenticação com Firebase
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, usuario)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                // Login bem-sucedido, redirecionar para a tela de pedidos
-                                startActivity(Intent(this, OrderActivity::class.java))
-                                finish()
-                            } else {
-                                // Tratar falha na autenticação
-                                Toast.makeText(this, "Autenticação falhou.", Toast.LENGTH_SHORT).show()
-                            }
+            if (isValidInput(email, senha)) {
+                // Autenticação com Firebase
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Login bem-sucedido, redirecionar para a tela de pedidos
+                            startActivity(Intent(this, PedidoActivity::class.java))
+                            finish()
+                        } else {
+                            // Tratar falha na autenticação
+                            Toast.makeText(this, "Falha na autenticação. Verifique suas credenciais.", Toast.LENGTH_SHORT).show()
                         }
-                } else {
-                    // Informar ao usuário que os campos são inválidos
-                    Toast.makeText(this, "Preencha todos os campos corretamente.", Toast.LENGTH_SHORT).show()
-                }
+                    }
+            } else {
+                // Informar ao usuário que os campos são inválidos
+                Toast.makeText(this, "Preencha todos os campos corretamente.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-        private fun isValidInput(email: String, usuario: String): Boolean {
-            return email.isNotEmpty() && usuario.isNotEmpty()
-        }
+    private fun isValidInput(email: String, senha: String): Boolean {
+        return email.isNotBlank() && senha.isNotBlank()
     }
 }
+
--- app/src/main/java/com/example/app_pedidosonline/MainActivity.kt
+++ app/src/main/java/com/example/app_pedidosonline/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.app_pedidosonline
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIniciar = findViewById<Button>(R.id.btnIniciar)
+
+        btnIniciar.setOnClickListener {
+            // Ao clicar no botão Iniciar, vá para a tela de login
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
